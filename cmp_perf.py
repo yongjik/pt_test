d4f262b073e2e76941fdcb0cdbf6fdae15ec334b
yongjik <yongjik@gmail.com>
2018-01-10 10:39:11 -0800

    Better marks for each line.

--- cmp_perf.py
+++ cmp_perf.py
@@ -2,12 +2,22 @@
 #
 # Compare the relative durations of test cases.
 
+import argparse
 import re
 import sys
 
-files = sys.argv[1:]
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--base', type=str, nargs='+')
+arg_parser.add_argument('--exp', type=str, nargs='+')
+
+argv = arg_parser.parse_args()
+
+files = argv.base + argv.exp
+base_cnt = len(argv.base)
 assert len(files) >= 2
 files = [open(f) for f in files]
+
+stat = {}
 
 while True:
     lines = [f.readline() for f in files]
@@ -28,13 +38,44 @@ while True:
         continue
 
     durations = [dur.group(1) for dur in durations]
-    base = float(durations[0])
-    diffs = [(float(dur) - base) / base for dur in durations[1:]]
-    important = any(abs(diff) > 0.1 for diff in diffs)
+    dur_vals = [float(d) for d in durations]
 
-    print('%5s  %7s -> %s (%s) %s' %
-          ('!!!!!' if important else '     ',
-           base,
-           ' '.join('%7s' % dur for dur in durations[1:]),
-           ' '.join('%+6.2f%%' % (diff * 100.0) for diff in diffs),
+    base_min = min(dur_vals[:base_cnt])
+    exp_min = min(dur_vals[base_cnt:])
+
+    # Relative difference between the best base/exp times.
+    best_diff = (exp_min - base_min) / base_min
+
+    if best_diff >= 0.2:
+        verdict = '0BAD'
+    elif best_diff >= 0.1:
+        verdict = '1Bad'
+    elif best_diff >= 0.05:
+        verdict = '2bad'
+    elif best_diff <= -0.2:
+        verdict = '6GOOD'
+    elif best_diff <= -0.1:
+        verdict = '5Good'
+    elif best_diff <= -0.05:
+        verdict = '4good'
+    else:
+        verdict = '3'
+
+    stat[verdict] = stat.get(verdict, 0) + 1
+
+    important = abs(best_diff) > 0.05
+
+    diffs = [(d - base_min) / base_min for d in dur_vals]
+    #diff_strs = ['%s=%+6.2f%%' % (d, diff * 100.0) for d, diff in zip(durations, diffs)]
+    diff_strs = ['%+6.2f%%' % (diff * 100.0) for diff in diffs]
+
+    print('%-5s  %.3f -> %.3f (%+6.2f%%) [%s / %s] %s' %
+          (verdict[1:],
+           base_min, exp_min, best_diff * 100.0,
+           ' '.join(diff_strs[:base_cnt]),
+           ' '.join(diff_strs[base_cnt:]),
            lines[0]))
+
+sys.stderr.write('=====\n')
+for key in sorted(stat):
+    sys.stderr.write('  %-5s : %5d\n' % (key[1:], stat[key]))
